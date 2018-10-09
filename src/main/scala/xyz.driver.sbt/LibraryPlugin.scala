1c624d4de012b2fd22602a94b95e75aa70da3946
Jakob Odersky <jakob@driver.xyz>
2018-10-09 15:30:25 -0700

    Revert "Remove duplicate snapshot resolver"
    This reverts commit cee9582b19c50e7d908d47f699442de0c14ca33f.

--- src/main/scala/xyz.driver.sbt/LibraryPlugin.scala
+++ src/main/scala/xyz.driver.sbt/LibraryPlugin.scala
@@ -18,7 +18,8 @@ object LibraryPlugin extends AutoPlugin {
   import autoImport._
 
   lazy val repositorySettings: Seq[Setting[_]] = Seq(
-    resolvers += "releases" at "https://drivergrp.jfrog.io/drivergrp/releases"
+    resolvers += "releases" at "https://drivergrp.jfrog.io/drivergrp/releases",
+    resolvers += "snapshots" at "https://drivergrp.jfrog.io/drivergrp/snapshots"
   )
 
   lazy val publicationSettings: Seq[Setting[_]] = Seq(
